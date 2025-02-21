e60cc2db05b003ba877c56baaef309b77015b90f
ManuelWestermeier <westermeier111@gmail.com>
2025-02-22 00:17:07 +0100

    groups

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import { areSetAndTheSameType as rset } from "are-set";
 import { createServer } from "wsnet-server";
 import * as crypto from '../utils/crypto.js'
 import fs from "fs";
-import { error } from "console";
 
-const users = JSON.parse(fs.readFileSync("./server/data/data.txt"));
+const allowedMemberTypes = ["client", "admin", "listener"];
+
+const users = JSON.parse(fs.readFileSync("./server/data/users.txt"));
+const groups = {}
 
 setInterval(() => {
   const data = {};
@@ -20,6 +22,37 @@ setInterval(() => {
 createServer({ port: 28028 }, async (client) => {
   let isAuth = false;
   let currentUser = false;
+
+  client.onGet("group:create", data => {
+    if (!isAuth) return;
+
+    if (!rset(data, [["name", "string"], ["members", "object"]])) {
+      return { error: "Invalid data format" };
+    }
+
+    const id = crypto.randomBytes(5);
+
+    const { name, members } = data;
+
+    for (const key in members) {
+      if (!allowedMemberTypes.includes(members[key])) {
+        return { error: "wrong type in members" };
+      }
+    }
+
+    if (groups[id]) return { error: "server error" };
+
+    groups[id] = {
+      name,
+      members,
+      messages: [],
+      joinPassword: crypto.randomBytes(5),
+    };
+  });
+  client.onGet("group:join", data => { });
+  client.onGet("group:send", data => { });
+  client.onGet("group:leave", data => { });
+  client.onGet("group:delete", data => { });
 
   client.onGet("update-user-data", data => {
     if (!isAuth) return { error: "not auth" };
@@ -69,7 +102,7 @@ createServer({ port: 28028 }, async (client) => {
       passwordHash: crypto.hash(passwordHash),
       data: userData,
       clients: [client],
-      groups: {},
+      unread: {},
     };
 
     return { error: false, created: true };
