57bc37462afe3cb409e63f7c90a319fefa1758df
Manuel Westermeier <150041048+ManuelWestermeier@users.noreply.github.com>
2025-02-21 09:31:58 +0000

    new

--- server/index.js
+++ server/index.js
@@ -1,76 +1,10 @@
+import { areSetAndTheSameType as rset } from "are-set"
 import { createServer } from "wsnet-server";
-
-const users = {
-  user1: {
-    name: "",
-    icon: "",
-    description: "",
-    posts: [
-      {
-        type: "text" || "image" || "video" || "audio",
-        data: "",
-        date: "",
-      },
-    ],
-    publicAllowed: "name" || "icon" || "description" || "posts",
-    contacts: [
-      { id: "", allowed: "name" || "icon" || "description" || "posts" },
-    ],
-    groups: [""],
-    password: "",
-  },
-};
-
-const chats = {
-  chat1: {
-    key: "",
-    name: "",
-    icon: "",
-    description: "",
-    isOpen: false,
-    users: [
-      {
-        type: "",
-        id: "",
-        lastRead: "",
-        lastIncome: "",
-      },
-    ],
-    messages: [
-      {
-        type:
-          "text" ||
-          "iframe" ||
-          "contact" ||
-          "email" ||
-          "image" ||
-          "video" ||
-          "audio" ||
-          "file" ||
-          "opinion-call" ||
-          "date" ||
-          "location" ||
-          "invention",
-        data: "",
-        id: "",
-        time: "",
-        comments: [
-          {
-            type: "text" || "image" || "emoji",
-            data: "",
-            from: "",
-            id: "",
-          },
-        ],
-        commented: false || "",
-        from: "",
-      },
-    ],
-  },
-};
 
 //create the websocket server on port 8080
 createServer({ port: 28028 }, async (client) => {
   var isAuth = false;
-  client.onParams((data) => {});
+  client.onGet("auth", data => {
+    if (!rset(data, [["user", "string"], ["password", "string"]])) return;
+  });
 });
